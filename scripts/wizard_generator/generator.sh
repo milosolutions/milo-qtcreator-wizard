83f10ed8fb73e30b7b004f068343c3a906a2a2ef
Wojtek <wojtek85@gmx.com>
2019-01-10 10:54:53 +0100

    add checkboxes support

--- scripts/wizard_generator/generator.sh
+++ scripts/wizard_generator/generator.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 MODULES=$(ls -d ../../packages/*/)
 
+
+checkboxes=""
 for MODULE in $MODULES
 do
     filename=$(basename -- "$MODULE")
-    extension="${filename##*.}"
-    echo "$filename"
-    echo "$extension"
+    moduleName="${filename##*.}"
+    #echo "$filename"
+    checkboxes="$checkboxes"\n$(sed -e "s;%moduleName%;$moduleName;g" checkbox.in)
 done
-
+echo "$checkboxes"
 cat header.in > wizard.json
 cat options.in >> wizard.json
 cat pages.in >> wizard.json
