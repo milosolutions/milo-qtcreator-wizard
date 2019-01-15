3b37f1ce2388a3c9a07a07e327608549cbfd43d7
Wojciech Knieć <wkniec@milosolutions.com>
2019-01-15 14:16:06 +0100

    add gitlab ci support

---
+++ scripts/wizard_generator/build_and_upload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+OUTPUT=./build/miloWizardinstaller_$(date +%Y.%m.%d).run
+DOMAIN=https://seafile.milosolutions.com
+REPO=$MILOCODEDATABASE_SEAFILE_REPO
+./scripts/wizard_generator/create_installer.sh $OUTPUT /home/tools/Qt-OpenSource/Tools/QtInstallerFramework/3.0/bin/binarycreator
+./scripts/upload_to_seafile.sh -f $OUTPUT -s $DOMAIN -r $REPO -t $SEAFILE_TOKEN
