7b34ef0b46f5bb1fb5898b093083b98eb056c4d4
Wojciech Knieć <wkniec@milosolutions.com>
2019-01-22 08:51:21 +0100

    change installer name

--- scripts/wizard_generator/build_and_upload.sh
+++ scripts/wizard_generator/build_and_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT=./build/miloWizardinstaller_$(date +%Y.%m.%d).run
+OUTPUT=./build/miloWizardInstaller_$(date +%Y.%m.%d).run
 DOMAIN=https://seafile.milosolutions.com
 REPO=$MILOCODEDATABASE_SEAFILE_REPO
 ./scripts/wizard_generator/create_installer.sh $OUTPUT /home/tools/Qt-OpenSource/Tools/QtInstallerFramework/3.0/bin/binarycreator
