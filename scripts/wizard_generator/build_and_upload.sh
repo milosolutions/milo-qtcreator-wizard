1fded35e4aae36f6e983a5f908e4c03a17d302ee
Wojciech Knieć <wkniec@milosolutions.com>
2019-01-22 12:04:09 +0100

    remove unnecessary scripts

--- scripts/wizard_generator/build_and_upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-OUTPUT=./build/miloWizardInstaller_$(date +%Y.%m.%d).run
-DOMAIN=https://seafile.milosolutions.com
-REPO=$MILOCODEDATABASE_SEAFILE_REPO
-./scripts/wizard_generator/create_installer.sh $OUTPUT /home/tools/Qt-OpenSource/Tools/QtInstallerFramework/3.0/bin/binarycreator
-./scripts/upload_to_seafile.sh -f $OUTPUT -s $DOMAIN -r $REPO -t $SEAFILE_TOKEN
